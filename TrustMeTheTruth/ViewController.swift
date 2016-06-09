379458aee706567de5881fe9b2822a9f3afb7574
GitKels <mikel@swiftogo.com>
2016-06-09 09:53:16 -0400

    connected Text Field as an Outlet

--- TrustMeTheTruth/ViewController.swift
+++ TrustMeTheTruth/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionTxtField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
